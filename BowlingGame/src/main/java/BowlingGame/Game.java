5f54e1330b7107006d03c6a211afdf9455bd45a3
KoheiOkamoto <koko72.flavor@gmail.com>
2020-06-10 00:15:00 +0900

    written the first test.

--- BowlingGame/src/main/java/BowlingGame/Game.java
+++ BowlingGame/src/main/java/BowlingGame/Game.java
@@ -1,4 +1,11 @@
 package BowlingGame;
 
 public class Game {
+
+	public void roll(int i) {
+	}
+
+	public int score() {
+		return 0;
+	}
 }
--- BowlingGame/src/test/java/BowlingGame/GameTest.java
+++ BowlingGame/src/test/java/BowlingGame/GameTest.java
@@ -1,16 +1,20 @@
 package BowlingGame;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.stream.IntStream;
+
 import org.junit.Test;
 
-import junit.framework.Assert;
-
-import static org.junit.Assert.*;
-
 public class GameTest {
-	
-    @Test 
-    public void testGutterGame() {
-        Game game = new Game();
-        Assert.fail();
-    }
+
+	@Test
+	public void testGutterGame() {
+		Game game = new Game();
+		IntStream.rangeClosed(1, 20).forEach(i -> game.roll(0));
+		int actual = game.score();
+		int expected = 0;
+		assertThat(actual, is(expected));
+	}
 }
