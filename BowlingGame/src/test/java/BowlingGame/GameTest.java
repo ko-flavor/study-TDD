3947eebb94794d879c24cdcaa8753e19176c939d
KoheiOkamoto <koko72.flavor@gmail.com>
2020-06-10 00:09:13 +0900

    modify files automatically made by gradle.

---
+++ BowlingGame/src/test/java/BowlingGame/GameTest.java
@@ -0,0 +1,16 @@
+package BowlingGame;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+	
+    @Test 
+    public void testGutterGame() {
+        Game game = new Game();
+        Assert.fail();
+    }
+}
