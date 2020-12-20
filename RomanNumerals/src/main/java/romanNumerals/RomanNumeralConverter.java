f79628deeebeb83df5e2941fa327a49b04c03150
KoheiOkamoto <koko72.flavor@gmail.com>
2020-12-20 14:37:49 +0900

    written second test, implemented and refactor.

--- RomanNumerals/src/main/java/romanNumerals/RomanNumeralConverter.java
+++ RomanNumerals/src/main/java/romanNumerals/RomanNumeralConverter.java
@@ -1,7 +1,11 @@
 package romanNumerals;
 
+import java.util.stream.IntStream;
+
 public class RomanNumeralConverter {
-	public String convert(int i) {
-		return "I";
+	public String convert(int number) {
+		StringBuilder builder = new StringBuilder();
+		IntStream.rangeClosed(1, number).forEach(i -> builder.append("I"));
+		return builder.toString();
 	}
 }
--- RomanNumerals/src/test/java/romanNumerals/RomanNumeralConverterTest.java
+++ RomanNumerals/src/test/java/romanNumerals/RomanNumeralConverterTest.java
@@ -6,10 +6,18 @@ import org.junit.jupiter.api.Test;
 
 public class RomanNumeralConverterTest {
 
+	RomanNumeralConverter converter = new RomanNumeralConverter();
+
 	@Test
 	public void test_one_is_converted_to_I() {
-		RomanNumeralConverter converter = new RomanNumeralConverter();
-		String result = converter.convert(1);
+		String result = this.converter.convert(1);
 		assertThat(result).isEqualTo("I");
 	}
+
+	@Test
+	public void test_two_is_converted_to_II() {
+		String result = this.converter.convert(2);
+		assertThat(result).isEqualTo("II");
+	}
+
 }
